5233be9eb7c44c3555e0bafcda7cd280ac76a972
Rupert <rupertatangang@gmail.com>
2026-01-28 00:39:46 -0500

    wdjn

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -81,7 +81,7 @@ module "eks" {
   ##############################################
   eks_managed_node_group_defaults = {
     ami_type       = "AL2023_x86_64_STANDARD"
-    instance_types = ["t3.medium"]
+    instance_types = ["t2.medium"]
 
     min_size     = 3
     max_size     = 5
